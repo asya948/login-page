bdb8a96300b5e457b075eaecd917337f50e573e5
Asya <ahambardzumyan2018@mail.ru>
2026-02-01 21:33:24 +0400

    i make web project file add new project file

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,29 @@
 import 'dotenv/config'
-import express from 'express';
-import cors from 'cors';
-const app=express();
-app.use(cors());
+import express from 'express'
+import cors from 'cors'
 
-let values=[]
-app.get('/',(req,res)=>{
-res.json(values);
+const app = express()
+
+app.use(cors())
+app.use(express.json()) // ✅ ՊԱՐՏԱԴԻՐ
+
+let values = []
+
+app.get('/', (req, res) => {
+    res.json(values)
 })
 
-app.post('/add',(req,res)=>{
-let value={
-    id:Date.now(),
-    title:req.body.value,
-}
-values.push(value)
-    res.json(values);
+app.post('/add', (req, res) => {
+    const value = {
+        id: Date.now(),
+        title: req.body.title,
+        body: req.body.body
+    }
+
+    values.push(value)
+    res.json(values)
 })
-app.listen(process.env.APP_PORT||3000,()=>{
-    console.log('server started ');
+
+app.listen(process.env.APP_PORT || 3000, () => {
+    console.log('server started')
 })
