dce414f3e7d9eb8f2b906ad5bc9cb8884aacdd11
Asya <ahambardzumyan2018@mail.ru>
2026-02-02 15:12:17 +0400

    i make web project file add new project file I added other information. Add new fetch

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,11 @@ import express from 'express'
 import cors from 'cors'
 import session from 'express-session'
 
+
 const app = express()
 
 app.use(express.json())
-
+app.use(express.urlencoded({ extended: true }))
 app.use(
     session({
         secret: 'mySecretKey123',
@@ -20,16 +21,20 @@ app.use(
 )
 
 app.use(cors())
-app.use(express.json())
+
 
 let users = []
 
-app.get('/login', (req, res) => {
-   const { login, password } = req.query
-  let usersValues=  users.find(user => user.login === login && user.password === password)
-    if(usersValue){
-        res.session.user = usersValues
-        return res.json(usersValues)
+app.post('/login', (req, res) => {
+   const { login, password } = req.body
+  let usersValues=  users.find(user => user.login.toLowerCase() === login.toLowerCase() && user.password === password)
+    if(usersValues){
+        let newUser = {
+            ...usersValues,
+        }
+        delete newUser.password
+        req.session.user = newUser
+        return res.json(newUser)
     }else{
        res.json({
            message: 'invalid login password',
@@ -38,6 +43,53 @@ app.get('/login', (req, res) => {
 
     }
 })
+
+app.get('/profile', (req, res) =>  {
+   if(req.session.user){
+       return res.json(req.session.user)
+    }else{
+        res.json({
+            message: 'not logged in',
+        })
+
+
+    }
+})
+
+app.get('/logout', (req, res) => {
+
+   if(req.session.user){
+       req.session.user = null
+       return res.json('logout users')
+   }
+    else{
+        res.json({
+            message: 'not logged out',
+        })
+
+
+    }
+})
+
+app.post('/register', (req, res) => {
+    const {name, email, password, login} = req.body
+    let val = {id: Date.now(), email, password, login, role: 'user'};
+    users.push(val)
+    let newValue = {
+        ...val
+    }
+    delete newValue.password
+    req.session.user ={
+        newValue
+    }
+
+    res.json(users)
+
+
+
+})
+
+
 
 
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,2 +1,17 @@
-const $=s=>document.querySelector("s");
-$('#')
+const $=s=>document.querySelector(s);
+
+fetch('http://localhost:3000/').then(res => res.json()).then(data => {
+    console.log(data)
+})
+
+$('#save').onclick = () => {
+    let fildEl=$('#loginForm')
+
+    fetch('http://localhost:3000/add', {
+        method: 'POST',
+        body: JSON.stringify({value:fildEl.value }),
+        headers: {'Content-Type': 'application/json'},
+    }).then(res => {
+    console.log(res)
+    })
+}
