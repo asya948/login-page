5279e2c51466b14d02a9299cf2c76184ecc2b967
Asya <ahambardzumyan2018@mail.ru>
2026-02-02 14:14:05 +0400

    i make web project file add new project file I added other information.

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,45 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import session from 'express-session'
 
 const app = express()
 
+app.use(express.json())
+
+app.use(
+    session({
+        secret: 'mySecretKey123',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            secure: false,
+            maxAge: 1000 * 60 * 60 * 24
+        }
+    })
+)
+
 app.use(cors())
-app.use(express.json()) // ✅ ՊԱՐՏԱԴԻՐ
+app.use(express.json())
 
-let values = []
+let users = []
 
-app.get('/', (req, res) => {
-    res.json(values)
-})
+app.get('/login', (req, res) => {
+   const { login, password } = req.query
+  let usersValues=  users.find(user => user.login === login && user.password === password)
+    if(usersValue){
+        res.session.user = usersValues
+        return res.json(usersValues)
+    }else{
+       res.json({
+           message: 'invalid login password',
+       })
 
-app.post('/add', (req, res) => {
-    const value = {
-        id: Date.now(),
-        title: req.body.title,
-        body: req.body.body
+
     }
-
-    values.push(value)
-    res.json(values)
 })
+
+
 
 app.listen(process.env.APP_PORT || 3000, () => {
     console.log('server started')
