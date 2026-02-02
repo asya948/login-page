6b137c03b5ab8e2a6d9b4ab42a99a01837b0f6da
Asya <ahambardzumyan2018@mail.ru>
2026-02-02 15:24:00 +0400

    I have almost finished one project

--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,33 @@
 const $=s=>document.querySelector(s);
 
-fetch('http://localhost:3000/').then(res => res.json()).then(data => {
+fetch('http://localhost:3000/profile').then(res => res.json()).then(data => {
     console.log(data)
 })
 
 $('#save').onclick = () => {
-    let fildEl=$('#loginForm')
+    const login = $('#login').value
+    const password = $('#password').value
+
 
     fetch('http://localhost:3000/add', {
         method: 'POST',
-        body: JSON.stringify({value:fildEl.value }),
+        body: JSON.stringify({login, password}),
         headers: {'Content-Type': 'application/json'},
     }).then(res => {
-    console.log(res)
+    console.table(res)
+    })
+}
+$('#register').onclick = () => {
+    const login = $('#login').value
+    const email = $('#email').value
+    const password = $('#password').value
+
+
+    fetch('http://localhost:3000/register', {
+        method: 'POST',
+        body: JSON.stringify({login, email, password }),
+        headers: {'Content-Type': 'application/json'},
+    }).then(res => {
+    console.table(res)
     })
 }
